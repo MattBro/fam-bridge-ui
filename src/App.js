676ad92d06c6d9a76a3cd5f02470c37161b6353e
NEXUS\mbrooker <mbrooker@commercehub.com>
2018-08-26 14:12:09 -0400

    Create a css folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 import { Switch, Route, Link } from 'react-router-dom'
 import Login from './login'
 import Signup from './signup'
